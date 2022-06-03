cc061d1988c655a2cfd8a90aa08c9ad7ff85f9d0
Matt Ray <sleepingbag5927@gmail.com>
2022-06-02 21:37:22 -0500

    Rough calendar in component

--- src/components/calendar/Calendar.js
+++ src/components/calendar/Calendar.js
@@ -1,11 +1,53 @@
-import "./calendar.css"
+import "./calendar.css";
 
 function Calander() {
-    return ( 
-        <div className="calendar-wrapper">
-            <h3>Calendar</h3>
-        </div>
-     );
+  return (
+    <div class="calendar-wrapper">
+      <h1>December 2020</h1>
+      <ol class="calendar">
+        <li class="day-name">Sun</li>
+        <li class="day-name">Mon</li>
+        <li class="day-name">Tue</li>
+        <li class="day-name">Wed</li>
+        <li class="day-name">Thu</li>
+        <li class="day-name">Fri</li>
+        <li class="day-name">Sat</li>
+
+        <li class="first-day day-number">1</li>
+
+        <li className="day-number">2</li>
+        <li className="day-number">3</li>
+        <li className="day-number">4</li>
+        <li className="day-number">5</li>
+        <li className="day-number">6</li>
+        <li className="day-number">7</li>
+        <li className="day-number">8</li>
+        <li className="day-number">9</li>
+        <li className="day-number">10</li>
+        <li className="day-number">11</li>
+        <li className="day-number">12</li>
+        <li className="day-number">13</li>
+        <li className="day-number">14</li>
+        <li className="day-number">15</li>
+        <li className="day-number">16</li>
+        <li className="day-number">17</li>
+        <li className="day-number">18</li>
+        <li className="day-number">19</li>
+        <li className="day-number">20</li>
+        <li className="day-number">21</li>
+        <li className="day-number">22</li>
+        <li className="day-number">23</li>
+        <li className="day-number">24</li>
+        <li className="day-number">25</li>
+        <li className="day-number">26</li>
+        <li className="day-number">27</li>
+        <li className="day-number">28</li>
+        <li className="day-number">29</li>
+        <li className="day-number">30</li>
+        <li className="day-number">31</li>
+      </ol>
+    </div>
+  );
 }
 
 export default Calander;
