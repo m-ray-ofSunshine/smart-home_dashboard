76e83b5ca7aa27ff617728fe13e916b7c6acd818
Matt Ray <sleepingbag5927@gmail.com>
2023-02-11 17:42:35 -0600

    Refactored Calendar component and made utils file

--- client/src/components/calendar/Calendar.js
+++ client/src/components/calendar/Calendar.js
@@ -1,69 +1,24 @@
 import { useEffect, useState } from "react";
 import "./calendar.css";
-import { getCalendar } from "../../utils/calendarUtils";
+import { getCalendar, Month, generateDays } from "../../utils/calendarUtils";
 
 function Calander({ currentMonth }) {
   const [events, setEvents] = useState();
 
+  
+  const today = new Date();
+  
+    const month = new Month(today);
+    
+    const nextMonth = new Month(new Date(today.setMonth(today.getMonth() + 1)));    
+  
   useEffect(() => {
     const apiCall = async () => {
-      const calEvents = await getCalendar();
+      const calEvents = await getCalendar(month.firstDateISO, month.lastDateISO);
       setEvents(calEvents);
     };
     apiCall();
-  }, []);
-
-  const today = new Date();
-  function Month(date) {
-    this.today = date;
-    this.monthName = this.today.toLocaleString("default", { month: "long" });
-    this.lastDayOfMonth = new Date(
-      this.today.getFullYear(),
-      this.today.getMonth() + 1,
-      0
-    ).getDate();
-    this.firstDayOfWeek = new Date(
-      this.today.getFullYear(),
-      this.today.getMonth(),
-      1
-    ).getDay();
-    this.firstDayStyle = { gridColumnStart: this.firstDayOfWeek + 1 };
-  }
-  const month = new Month(today);
-
-  const nextMonth = new Month(new Date(today.setMonth(today.getMonth() + 1)));
-  console.log(month);
-  const generateDays = (month, events) => {
-    let days = [];
-    for (let i = 1; i <= month.lastDayOfMonth; i++) {
-      if (i === 1) {
-        days.push(
-          <li
-            key={i}
-            id="first-day"
-            className="day-number"
-            style={month.firstDayStyle}
-          >
-            {i}
-          </li>
-        );
-      } else if (currentMonth && month.today.getDate() === i) {
-        days.push(
-          <li key={i} className="currentDay day-number">
-            <p>{i}</p>
-          </li>
-        );
-      } else {
-        days.push(
-          <li key={i} className="day-number">
-            <p>{i}</p>
-          </li>
-        );
-      }
-    }
-    return days;
-  };
-
+  }, [month.firstDateISO, month.lastDateISO]);
   return (
     <div className="calendar-wrapper">
       <button onClick={()=>{console.log(events);}}>search</button>
@@ -80,7 +35,7 @@ function Calander({ currentMonth }) {
         <li className="day-name">Sat</li>
 
         {events &&
-        generateDays((currentMonth ? month : nextMonth), events)}
+        generateDays((currentMonth ? month : nextMonth), currentMonth, events)}
       </ol>
     </div>
   );
--- client/src/utils/calendarUtils.js
+++ client/src/utils/calendarUtils.js
@@ -1,8 +1,69 @@
 
-export const getCalendar = async (req, res) => {
-  return await fetch("/api/calendar")
-      .then(res => res.json());
+export const getCalendar = async (startDate, endDate, req, res) => {
+  return await fetch(`/api/calendar/${startDate}/${endDate}`)
+  .then(res => res.json())
   
+};
+export function Month(date) {
+  //Date format of today
+  this.today = date;
+  //Month name in long string form
+  this.monthName = this.today.toLocaleString("default", { month: "long" });
+  //last day of month
+  this.lastDayOfMonth = new Date(
+    this.today.getFullYear(),
+    this.today.getMonth() + 1,
+    0
+  ).getDate();
+  //Day of week for first day in the month
+  this.firstDayOfWeek = new Date(
+    this.today.getFullYear(),
+    this.today.getMonth(),
+    1
+    ).getDay();
+    //where the calendar starts
+    this.firstDayStyle = { gridColumnStart: this.firstDayOfWeek + 1 };
+    //first day of month
+    this.firstDateISO = new Date(
+      this.today.getFullYear(),
+      this.today.getMonth(),
+      1
+      ).toISOString();
+    this.lastDateISO = new Date(
+      this.today.getFullYear(),
+      this.today.getMonth() + 1,
+    0
+      ).toISOString();
+  };
+  export const generateDays = (month, currentMonth, events) => {
+    let days = [];
+    for (let i = 1; i <= month.lastDayOfMonth; i++) {
+      if (i === 1) {
+        days.push(
+          <li
+          key={i}
+          id="first-day"
+          className="day-number"
+          style={month.firstDayStyle}
+          >
+          {i}
+        </li>
+      );
+    } else if (currentMonth && month.today.getDate() === i) {
+      days.push(
+        <li key={i} className="currentDay day-number">
+          <p>{i}</p>
+        </li>
+      );
+    } else {
+      days.push(
+        <li key={i} className="day-number">
+          <p>{i}</p>
+        </li>
+      );
+    }
+  }
+  return days;
 };
 
  
