9373382b9f970af7ae1c62e92bc808a524d08edf
Matt Ray <sleepingbag5927@gmail.com>
2023-02-20 20:59:29 -0600

    Finished Date styling

--- client/src/components/todaysDate/TodaysDate.js
+++ client/src/components/todaysDate/TodaysDate.js
@@ -1,10 +1,23 @@
 import "./TodaysDate.css"
 function TodaysDate() {
-    return ( 
+
+var weekday = new Intl.DateTimeFormat('en-US', {
+    weekday: "long"
+}).formatToParts(new Date())[0].value
+var date = new Intl.DateTimeFormat('en-us', {
+     year: "numeric",
+     month: "numeric",
+     day: "numeric",
+}).format(new Date())
+
+    return (
         <div className="date-wrapper">
-            {new Date().toLocaleDateString()}
+            <h1 className="date-text">
+                <span className="weekday-text">{weekday}</span> , {date}
+            </h1>
+
         </div>
-     );
+    );
 }
 
 export default TodaysDate;
