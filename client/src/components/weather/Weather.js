f9bbb06df2f52d3abe3285b3db04083e512cf044
Matt Ray <sleepingbag5927@gmail.com>
2023-02-27 21:59:39 -0600

    weather comp hooked up to api

--- client/src/components/weather/Weather.js
+++ client/src/components/weather/Weather.js
@@ -1,10 +1,62 @@
+import { useEffect, useState } from 'react'
+import env from "react-dotenv";
 import './Weather.css'
 function Weather() {
-    return ( 
-        <div className="weather-wrapper">
-            WEATHER
-        </div>
-     );
+    const api_key = env.weather_api_key
+    const lat = env.latitude
+    const long = env.longitude
+
+    const [weatherData, setWeatherData] = useState();
+
+    function getIconImage(iconCode) {
+        var iconUrl = "https://openweathermap.org/img/w/" + iconCode + ".png";
+        var image = <img src={iconUrl} alt={weatherData.weather[0].description} width="75" height="75" ></img>
+        return image;
+    }
+
+    useEffect(() => {
+        const getWeatherData = async () => {
+            return await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${long}&units=imperial&exclude=minutely,hourly,daily&appid=${api_key}`)
+                .then(res => res.json())
+        };
+
+        if (!weatherData) {
+            getWeatherData()
+                .then(data => {
+                    setWeatherData(data.current)
+                });
+        }
+    }, [weatherData, lat, long, api_key]);
+    console.log(weatherData);
+    return (
+        <>
+            {weatherData &&
+                <div className="weather-wrapper">
+
+                    <div className='current-temp'>
+                        <div className='temp'>{weatherData.temp} f &#176;</div>
+                        <div>{getIconImage(weatherData.weather[0].icon)}</div>
+                    </div>
+                    <div className='hourly-weather'>
+                        <div>hour weather</div>
+                        <div>hour weather</div>
+                        <div>hour weather</div>
+                    </div>
+                    <div className='weather-other'>
+                        <div>Wind speed: <span>{weatherData.wind_speed} mph</span></div>
+                        <div>Feels like: <span>{weatherData.feels_like} f &#176;</span></div>
+                        <div>Sunrise: <span>{new Date(weatherData.sunrise*1000).toLocaleTimeString()}</span></div>
+                        <div>Sunset: <span>{new Date(weatherData.sunset*1000).toLocaleTimeString()}</span></div>
+                    </div>
+                    <div className='hourly-weather'>
+                        <div>day weather</div>
+                        <div>day weather</div>
+                        <div>day weather</div>
+                    </div>
+                </div>
+            }
+        </>
+    );
 }
 
 export default Weather;
