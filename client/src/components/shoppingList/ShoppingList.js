8730bcd8feb3b3b44cff58f6acc390c8dec01a3b
Matt Ray <sleepingbag5927@gmail.com>
2024-12-14 19:01:36 -0600

    Add shooping list logic

--- client/src/components/shoppingList/ShoppingList.js
+++ client/src/components/shoppingList/ShoppingList.js
@@ -1,8 +1,44 @@
+import { useEffect, useState } from 'react'
 import "./ShoppingList.css"
+
 function ShoppingList() {
+    const api_key = process.env.REACT_APP_TODO_LIST_API_KEY
+    const todo_list_api_url = process.env.REACT_APP_TODO_LIST_URL
+
+    const [todoListData, setTodoListData] = useState();
+
+    useEffect(() => {
+
+        const getToDoListData = async () => {
+            const options = {
+                "method": 'GET',
+                "headers": {
+                    'Content-Type': 'application/json', 
+                    'x-api-key': api_key 
+                }
+            };
+            console.log(`${todo_list_api_url}/list`);
+            return await fetch(`${todo_list_api_url}/list`, options)
+                .then(res => res.json())
+                .catch(error => {
+                    console.log("ERROR-------------", error);
+                });
+        };
+
+        if (!todoListData) {
+            getToDoListData()
+                .then(data => {
+                    setTodoListData(data)
+                });
+        }
+    }, [todoListData]);
     return ( 
         <div className="shopping-list-wrapper">
             Shopping List
+            {todoListData &&
+            todoListData.map((item, index) => (
+                <p key={index}>{item.name}</p>
+            ))}
         </div>
      );
 }
