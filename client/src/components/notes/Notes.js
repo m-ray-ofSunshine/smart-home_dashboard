189e99299bd255d52337ec77cf9d50bc7543f376
Matt Ray <sleepingbag5927@gmail.com>
2024-12-27 21:56:09 -0600

    Update Notes component to use api

--- client/src/components/notes/Notes.js
+++ client/src/components/notes/Notes.js
@@ -1,10 +1,63 @@
+import { useEffect, useState } from 'react'
 import "./Notes.css"
 function Notes() {
-    return ( 
-        <div className="notes-wrapper">
-            Notes
+
+    const api_key = process.env.REACT_APP_AWS_API_KEY
+    const api_url = process.env.REACT_APP_AWS_API_URL
+
+    const [notesData, setNotesData] = useState();
+
+    useEffect(() => {
+
+        const getNotesData = async () => {
+            const options = {
+                "method": 'GET',
+                "headers": {
+                    'Content-Type': 'application/json',
+                    'x-api-key': api_key
+                }
+            };
+            return await fetch(`${api_url}/notes`, options)
+                .then(res => res.json())
+                .catch(error => {
+                    console.log("ERROR-------------", error);
+                });
+        };
+
+        if (!notesData) {
+            getNotesData()
+                .then(data => {
+                    setNotesData(data)
+                    
+                });
+            }
+        }, [notesData]);
+        
+        
+        const columnLimit = 6;
+    const columns = [];
+    if (notesData) {
+        for (let i = 0; i < notesData.length; i += columnLimit) {
+            columns.push(notesData.slice(i, i + columnLimit));
+        }
+    }
+
+    return (
+        <div className="notes-wrapper notes-container" >
+            <h1 className="notes-title" >Shopping List</h1>
+            <div className="notes-columns-container" >
+                {columns.map((column, colIndex) => (
+                    <div key={colIndex} className="notes-column" >
+                        {column.map((item, index) => (
+                            <div key={index} className="notes-list-item" >
+                                {item.name}
+                            </div>
+                        ))}
+                    </div>
+                ))}
+            </div>
         </div>
-     );
+    );
 }
 
 export default Notes;
